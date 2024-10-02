dcaab90d6f8b65ab583441d11e4d98e7417549eb
Spacplus <168353872+Spacplus@users.noreply.github.com>
2024-10-02 09:55:44 -0300

    Projeto Ficha 1.0

---
+++ ficha/CREATE TABLE characters (.sql
@@ -0,0 +1,8 @@
+CREATE TABLE characters (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    class VARCHAR(50),
+    level INT,
+    strength INT,
+    -- Adicionar mais atributos
+);
